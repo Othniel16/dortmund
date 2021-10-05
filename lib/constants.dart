2eddef6b6563b15079f1898e4e5ace9231d4a6b0
Othniel <othnielussher16@gmail.com>
2021-10-05 10:43:19 +0000

    📁 Add new file

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+class Images {
+  static const String logo = 'assets/images/logo.png';
+  static const String disruption = 'assets/images/disruption.jpg';
+  static const String woman = 'assets/images/woman.jpg';
+}
+
+class FontSizes {
+  static const large = 48.0;
+  static const regular = 16.0;
+}
