4f0981d5f8e634bfca0fa5fe0716c713513bfb49
Othniel <othnielussher16@gmail.com>
2021-10-05 10:43:23 +0000

    📁 Add new file

---
+++ lib/exports.dart
@@ -0,0 +1,14 @@
+export 'package:dortmund/app.dart';
+export 'package:dortmund/carousel.dart';
+export 'package:dortmund/constants.dart';
+export 'package:dortmund/widgets/header.dart';
+export 'package:dortmund/widgets/footer.dart';
+export 'package:dortmund/widgets/center_widget.dart';
+export 'package:carousel_slider/carousel_slider.dart';
+export 'package:dortmund/pages/hi_there/hi_there.dart';
+export 'package:dortmund/state_management/carousel_pages_provider.dart';
+
+
+// pub packages
+export 'package:provider/provider.dart';
+export 'package:expandable/expandable.dart';
