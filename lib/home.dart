1c2b7913d06038fd6f77fc1b6f8e5d99b89a50c2
Othniel <othnielussher16@gmail.com>
2021-10-06 00:40:10 +0000

    Extract home class to separate file

---
+++ lib/home.dart
@@ -0,0 +1,48 @@
+import 'package:dortmund/exports.dart';
+import 'package:flutter/material.dart';
+import 'package:dortmund/widgets/menu_widget.dart';
+
+final GlobalKey<SliderMenuContainerState> menuKey =
+    GlobalKey<SliderMenuContainerState>();
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 30.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Header(),
+            Expanded(
+              child: SliderMenuContainer(
+                key: menuKey,
+                hasAppBar: false,
+                sliderMenu: const MenuWidget(),
+                slideDirection: SlideDirection.TOP_TO_BOTTOM,
+                sliderMenuOpenSize: MediaQuery.of(context).size.height,
+                sliderMain: Container(
+                  color: Colors.black,
+                  child: const Carousel(),
+                ),
+              ),
+            ),
+            // Expanded(
+            //   child: Container(
+            //     color: Colors.black,
+            //     child: const Carousel(),
+            //   ),
+            // ),
+            const Footer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
