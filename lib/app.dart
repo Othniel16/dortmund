8beeb0f7c96f67b1fe8d8de47a7e54536976b236
Othniel <othnielussher16@gmail.com>
2021-10-06 00:39:36 +0000

    Extract Home class to separate file

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,8 @@
 import 'package:dortmund/exports.dart';
 import 'package:flutter/material.dart';
-import 'package:dortmund/pages/the_new_disruption/the_new_disruption.dart';
 
 class App extends StatelessWidget {
-  App({Key? key}) : super(key: key);
-
-  final List<Widget> pages = [
-    const TheNewDisruption(),
-    const HiThere(),
-    const ThankYou()
-  ];
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,52 +13,8 @@ class App extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.white,
-          body: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 30.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                const Header(),
-                Expanded(
-                  child: Container(
-                    color: Colors.black,
-                    child: const Carousel(),
-                  ),
-                ),
-                const Footer(),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ThankYou extends StatelessWidget {
-  const ThankYou({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const CenterWidget(
-      leftChild: Text(
-        'Hey 3!!',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 30.0,
-        ),
-      ),
-      rightChild: Center(
-        child: Text(
-          'Thank You!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 30.0,
-          ),
-        ),
+        theme: ThemeData(fontFamily: 'Cera Pro'),
+        home: const Home(),
       ),
     );
   }
