2affc45ea8d3441677c1d50a61cd0eefd1a412c1
Othniel <othnielussher16@gmail.com>
2021-10-06 00:39:55 +0000

    ⛴ Add new exports

--- lib/exports.dart
+++ lib/exports.dart
@@ -1,14 +1,17 @@
 export 'package:dortmund/app.dart';
+export 'package:dortmund/home.dart';
+export 'pages/thank_you/thank_you.dart';
 export 'package:dortmund/carousel.dart';
 export 'package:dortmund/constants.dart';
 export 'package:dortmund/widgets/header.dart';
 export 'package:dortmund/widgets/footer.dart';
 export 'package:dortmund/widgets/center_widget.dart';
 export 'package:carousel_slider/carousel_slider.dart';
-export 'package:dortmund/pages/hi_there/hi_there.dart';
+export 'package:dortmund/widgets/social_media_icons.dart';
 export 'package:dortmund/state_management/carousel_pages_provider.dart';
-
 
 // pub packages
 export 'package:provider/provider.dart';
 export 'package:expandable/expandable.dart';
+export 'package:flutter_bounce/flutter_bounce.dart';
+export 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
