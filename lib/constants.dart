13acb63ffd4780abaf4ce6cab55b95e208f3b123
Othniel <othnielussher16@gmail.com>
2021-10-06 00:39:45 +0000

    Add new constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,9 +2,18 @@ class Images {
   static const String logo = 'assets/images/logo.png';
   static const String disruption = 'assets/images/disruption.jpg';
   static const String woman = 'assets/images/woman.jpg';
+  static const String moon = 'assets/images/moon.png';
+  static const String facebook = 'assets/images/facebook.png';
+  static const String clouds = 'assets/images/clouds.png';
+  static const String shapes = 'assets/images/shapes.png';
+  static const String instagram = 'assets/images/instagram.png';
+  static const String twitter = 'assets/images/twitter.png';
+  static const String cloudAndTriangle = 'assets/images/cloud_and_triangle.png';
+  static const String twoPhones = 'assets/images/two_phones.png';
+  static const String hiThere = 'assets/images/woman_with_star_and_heart.png';
 }
 
 class FontSizes {
-  static const large = 48.0;
-  static const regular = 16.0;
+  static const large = 45.0;
+  static const regular = 18.0;
 }
