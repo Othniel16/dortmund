4b09687d7a34e75d9feadef2ac4a905a22b67e13
Othniel Ussher <othnielussher16@gmail.com>
2021-10-12 17:34:05 +0000

    Add mobile responsive

--- lib/pages/hi_there/hi_there.dart
+++ lib/pages/hi_there/hi_there.dart
@@ -41,78 +41,83 @@ submission to our quick survey is welcome  ❤''';
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
     final double screenHeight = MediaQuery.of(context).size.height;
-    return Stack(
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 30.0),
-            Text(
-              title,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: FontSizes.large,
+    return ResponsiveBuilder(builder: (context, sizingInformation) {
+      double titleSize =
+          sizingInformation.isMobile ? FontSizes.semiLarge : FontSizes.large;
+      return Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 30.0),
+              Text(
+                title,
+                style: TextStyle(color: Colors.white, fontSize: titleSize),
               ),
-            ),
-            const SizedBox(height: 30.0),
-            Text(
-              invite,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: FontSizes.regular,
-              ),
-            ),
-            const SizedBox(height: 20.0),
-            Container(
-              color: Colors.grey,
-              height: 1.5,
-              width: MediaQuery.of(context).size.width / 3,
-            ),
-            const SizedBox(height: 30.0),
-            const UserInfo(label: 'Age Group'),
-            const UserInfo(label: 'Country'),
-            const UserInfo(label: 'Gender'),
-            const UserInfo(label: 'Occupation'),
-            const SizedBox(height: 45.0),
-            Consumer<CarouselPagesProvider>(
-                builder: (context, pageProvider, child) {
-              return InkWell(
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 15.0,
-                    vertical: 10.0,
-                  ),
-                  decoration:
-                      BoxDecoration(border: Border.all(color: Colors.white)),
-                  child: Text(
-                    submit,
-                    style: const TextStyle(color: Colors.white),
-                  ),
+              const SizedBox(height: 30.0),
+              Text(
+                invite,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: FontSizes.regular,
                 ),
-                onTap: () {
-                  pageProvider.togglePageIndex(pageProvider.currentPage + 1);
-                  carouselController.animateToPage(
-                    pageProvider.currentPage,
-                    curve: Curves.linear,
-                  );
-                },
-              );
-            }),
-          ],
-        ),
-        Positioned(
-          right: screenWidth / 18,
-          bottom: screenHeight / 5,
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              maxWidth: screenWidth / 30,
-              maxHeight: screenWidth / 30,
-            ),
-            child: Image.asset(Images.moon),
+              ),
+              const SizedBox(height: 20.0),
+              Container(
+                color: Colors.grey,
+                height: 1.5,
+                width: MediaQuery.of(context).size.width / 3,
+              ),
+              const SizedBox(height: 30.0),
+              const UserInfo(label: 'Age Group'),
+              const UserInfo(label: 'Country'),
+              const UserInfo(label: 'Gender'),
+              const UserInfo(label: 'Occupation'),
+              const SizedBox(height: 45.0),
+              Consumer<CarouselPagesProvider>(
+                  builder: (context, pageProvider, child) {
+                return InkWell(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 15.0,
+                      vertical: 10.0,
+                    ),
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.white)),
+                    child: Text(
+                      submit,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  onTap: () {
+                    pageProvider.togglePageIndex(pageProvider.currentPage + 1);
+                    carouselController.animateToPage(
+                      pageProvider.currentPage,
+                      curve: Curves.linear,
+                    );
+                  },
+                );
+              }),
+            ],
           ),
-        ),
-      ],
-    );
+          Positioned(
+            right: screenWidth / 18,
+            bottom: screenHeight / 5,
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: sizingInformation.isMobile
+                    ? screenWidth / 15
+                    : screenWidth / 30,
+                maxHeight: sizingInformation.isMobile
+                    ? screenWidth / 15
+                    : screenWidth / 30,
+              ),
+              child: Image.asset(Images.moon),
+            ),
+          ),
+        ],
+      );
+    });
   }
 }
 
