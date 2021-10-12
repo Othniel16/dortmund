9aad1eb928b242e2e4d8d8cf8cc7a32a62fb04d4
Othniel Ussher <othnielussher16@gmail.com>
2021-10-12 17:34:30 +0000

    Add mobile responsiveness

--- lib/widgets/menu_widget.dart
+++ lib/widgets/menu_widget.dart
@@ -6,17 +6,21 @@ class MenuWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
-      height: MediaQuery.of(context).size.height,
-      child: Row(
-        children: const [
-          Expanded(flex: 4, child: LeftWidget()),
-          Spacer(),
-          Expanded(flex: 5, child: RightWidget()),
-        ],
-      ),
-    );
+    return ResponsiveBuilder(builder: (context, sizingInformation) {
+      return Container(
+        color: Colors.black,
+        height: MediaQuery.of(context).size.height,
+        child: Row(
+          children: [
+            sizingInformation.isMobile
+                ? Container()
+                : const Expanded(flex: 4, child: LeftWidget()),
+            sizingInformation.isMobile ? Container() : const Spacer(),
+            const Expanded(flex: 5, child: RightWidget()),
+          ],
+        ),
+      );
+    });
   }
 }
 
@@ -51,71 +55,89 @@ class RightWidget extends StatelessWidget {
     final double screenHeight = MediaQuery.of(context).size.height;
     return Padding(
       padding: EdgeInsets.only(right: screenWidth / 25, top: screenHeight / 35),
-      child: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 30.0),
-              Text(
-                comingSoon,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: FontSizes.large,
+      child: ResponsiveBuilder(builder: (context, sizingInformation) {
+        double titleSize =
+            sizingInformation.isMobile ? FontSizes.semiLarge : FontSizes.large;
+        return Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Column(
+              crossAxisAlignment: sizingInformation.isMobile
+                  ? CrossAxisAlignment.center
+                  : CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 30.0),
+                Text(
+                  comingSoon,
+                  style: TextStyle(color: Colors.white, fontSize: titleSize),
                 ),
-              ),
-              const SizedBox(height: 30.0),
-              Text(
-                launching,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: FontSizes.regular,
+                const SizedBox(height: 30.0),
+                Text(
+                  launching,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: FontSizes.regular,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20.0),
-              Container(
-                color: Colors.grey,
-                height: 1.5,
-                width: MediaQuery.of(context).size.width / 2.8,
-              ),
-              const SizedBox(height: 30.0),
-              Text(
-                launchDate,
-                style: const TextStyle(
-                  color: SiteColors.brown,
-                  fontSize: FontSizes.large,
+                const SizedBox(height: 20.0),
+                Container(
+                  color: Colors.grey,
+                  height: 1.5,
+                  width: MediaQuery.of(context).size.width / 2.8,
                 ),
-              ),
-            ],
-          ),
-
-          // moon
-          Positioned(
-            right: screenWidth / 20,
-            top: screenHeight / 30,
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                maxWidth: screenWidth / 30,
-                maxHeight: screenWidth / 30,
-              ),
-              child: Image.asset(Images.moon),
+                const SizedBox(height: 30.0),
+                Text(
+                  launchDate,
+                  style:
+                      TextStyle(color: SiteColors.brown, fontSize: titleSize),
+                ),
+                sizingInformation.isMobile
+                    ? Padding(
+                        padding: const EdgeInsets.only(top: 40.0),
+                        child: ConstrainedBox(
+                          constraints: BoxConstraints(
+                            maxWidth: MediaQuery.of(context).size.width / 4,
+                            maxHeight: MediaQuery.of(context).size.width / 4,
+                          ),
+                          child: Image.asset(Images.clouds),
+                        ),
+                      )
+                    : Container(),
+              ],
             ),
-          ),
 
-          // clouds
-          Positioned(
-            right: screenWidth / 25,
-            bottom: screenHeight / 4,
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                maxWidth: MediaQuery.of(context).size.width / 7,
-                maxHeight: MediaQuery.of(context).size.width / 5,
-              ),
-              child: Image.asset(Images.clouds),
-            ),
-          ),
-        ],
-      ),
+            // moon
+            sizingInformation.isMobile
+                ? Container()
+                : Positioned(
+                    right: screenWidth / 20,
+                    top: screenHeight / 30,
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxWidth: screenWidth / 30,
+                        maxHeight: screenWidth / 30,
+                      ),
+                      child: Image.asset(Images.moon),
+                    ),
+                  ),
+
+            // clouds
+            sizingInformation.isMobile
+                ? Container()
+                : Positioned(
+                    right: screenWidth / 25,
+                    bottom: screenHeight / 4,
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width / 7,
+                        maxHeight: MediaQuery.of(context).size.width / 5,
+                      ),
+                      child: Image.asset(Images.clouds),
+                    ),
+                  ),
+          ],
+        );
+      }),
     );
   }
 }
