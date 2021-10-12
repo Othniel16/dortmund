df55551264195b8e17867310ad809716af2e63c4
Othniel Ussher <othnielussher16@gmail.com>
2021-10-12 17:34:32 +0000

    Add mobile responsiveness

--- lib/widgets/social_media_icons.dart
+++ lib/widgets/social_media_icons.dart
@@ -2,13 +2,15 @@ import 'package:dortmund/exports.dart';
 import 'package:flutter/material.dart';
 
 class SocialMediaIcons extends StatelessWidget {
-  const SocialMediaIcons({Key? key}) : super(key: key);
+  final MainAxisAlignment? mainAxisAlignment;
+  const SocialMediaIcons({Key? key, this.mainAxisAlignment}) : super(key: key);
 
   final double imageSize = 30.0;
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment ?? MainAxisAlignment.start,
       children: [
         Bounce(
           duration: const Duration(milliseconds: 100),
