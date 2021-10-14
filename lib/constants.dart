678f6cfcd6e03ea71a35e083aaefcb966395a969
Othniel Ussher <othnielussher16@gmail.com>
2021-10-14 09:04:25 +0000

    Remove unused image references

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Images {
+  static const String aneteLusina = 'assets/images/anete_lusina.jpg';
   static const String logo = 'assets/images/logo.png';
   static const String disruption = 'assets/images/disruption.jpg';
   static const String woman = 'assets/images/woman.jpg';
-  static const String moon = 'assets/images/moon.png';
   static const String facebook = 'assets/images/facebook.png';
-  static const String clouds = 'assets/images/clouds.png';
-  static const String shapes = 'assets/images/shapes.png';
   static const String instagram = 'assets/images/instagram.png';
   static const String twitter = 'assets/images/twitter.png';
-  static const String cloudAndTriangle = 'assets/images/cloud_and_triangle.png';
   static const String twoPhones = 'assets/images/two_phones.png';
   static const String hiThere = 'assets/images/woman_with_star_and_heart.png';
 }
