bba18c56dd86655fc14d7d07dba53372ab981000
Othniel Ussher <othnielussher16@gmail.com>
2021-10-14 09:05:13 +0000

    Remove cartoony images

--- lib/pages/hi_there/hi_there.dart
+++ lib/pages/hi_there/hi_there.dart
@@ -1,4 +1,3 @@
-import 'package:dortmund/constants.dart';
 import 'package:dortmund/exports.dart';
 import 'package:flutter/material.dart';
 
@@ -19,105 +18,85 @@ class LeftWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double screenWidth = MediaQuery.of(context).size.width;
     final double screenHeight = MediaQuery.of(context).size.height;
     return Padding(
-      padding:
-          EdgeInsets.only(left: screenWidth / 30, bottom: screenHeight / 20),
-      child: Image.asset(Images.hiThere),
+      padding: EdgeInsets.only(bottom: screenHeight / 20),
+      child: Image.asset(Images.woman),
     );
   }
 }
 
-class RightWidget extends StatelessWidget {
+class RightWidget extends StatefulWidget {
   const RightWidget({Key? key}) : super(key: key);
 
+  @override
+  State<RightWidget> createState() => _RightWidgetState();
+}
+
+class _RightWidgetState extends State<RightWidget> {
   final String submit = 'SUBMIT';
+
   final String title = 'Hi There ...';
+
   final String invite = '''We’d like you to be a part of this project – your
 submission to our quick survey is welcome  ❤''';
+
+  late CarouselPagesProvider pagesProvider;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    pagesProvider = Provider.of<CarouselPagesProvider>(context, listen: false);
+  }
 
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
-    final double screenHeight = MediaQuery.of(context).size.height;
     return ResponsiveBuilder(builder: (context, sizingInformation) {
       double titleSize =
           sizingInformation.isMobile ? FontSizes.semiLarge : FontSizes.large;
-      return Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 30.0),
-              Text(
-                title,
-                style: TextStyle(color: Colors.white, fontSize: titleSize),
-              ),
-              const SizedBox(height: 30.0),
-              Text(
-                invite,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: FontSizes.regular,
-                ),
-              ),
-              const SizedBox(height: 20.0),
-              Container(
-                color: Colors.grey,
-                height: 1.5,
-                width: MediaQuery.of(context).size.width / 3,
-              ),
-              const SizedBox(height: 30.0),
-              const UserInfo(label: 'Age Group'),
-              const UserInfo(label: 'Country'),
-              const UserInfo(label: 'Gender'),
-              const UserInfo(label: 'Occupation'),
-              const SizedBox(height: 45.0),
-              Consumer<CarouselPagesProvider>(
-                  builder: (context, pageProvider, child) {
-                return InkWell(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 15.0,
-                      vertical: 10.0,
-                    ),
-                    decoration:
-                        BoxDecoration(border: Border.all(color: Colors.white)),
-                    child: Text(
-                      submit,
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  onTap: () {
-                    pageProvider.togglePageIndex(pageProvider.currentPage + 1);
-                    carouselController.animateToPage(
-                      pageProvider.currentPage,
-                      curve: Curves.linear,
-                    );
-                  },
-                );
-              }),
-            ],
-          ),
-          Positioned(
-            right: screenWidth / 18,
-            bottom: screenHeight / 5,
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                maxWidth: sizingInformation.isMobile
-                    ? screenWidth / 15
-                    : screenWidth / 30,
-                maxHeight: sizingInformation.isMobile
-                    ? screenWidth / 15
-                    : screenWidth / 30,
-              ),
-              child: Image.asset(Images.moon),
+      return Container(
+        padding: sizingInformation.isMobile
+            ? EdgeInsets.zero
+            : EdgeInsets.only(left: screenWidth / 25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30.0),
+            Text(
+              title,
+              style: TextStyle(color: Colors.white, fontSize: titleSize),
             ),
-          ),
-        ],
+            const SizedBox(height: 30.0),
+            Text(
+              invite,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: FontSizes.regular,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            Container(
+              color: Colors.grey,
+              height: 1.5,
+              width: MediaQuery.of(context).size.width / 3,
+            ),
+            const SizedBox(height: 30.0),
+            const UserInfo(label: 'Age Group'),
+            const UserInfo(label: 'Country'),
+            const UserInfo(label: 'Gender'),
+            const UserInfo(label: 'Occupation'),
+            const SizedBox(height: 45.0),
+            ActionButton(label: submit, onTap: onSubmitTap),
+          ],
+        ),
       );
     });
+  }
+
+  void onSubmitTap() {
+    pagesProvider.togglePageIndex(pagesProvider.currentPage + 1);
+    carouselController.animateToPage(pagesProvider.currentPage);
   }
 }
 
