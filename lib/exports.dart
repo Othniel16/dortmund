882fa4265d16ee9f3b8e459297b7b6a78671eba5
Othniel Ussher <othnielussher16@gmail.com>
2021-10-15 02:58:44 +0000

    🚢 Add new exports

--- lib/exports.dart
+++ lib/exports.dart
@@ -1,7 +1,6 @@
 export 'package:dortmund/app.dart';
 export 'package:dortmund/home.dart';
 export 'pages/thank_you/thank_you.dart';
-export 'package:dortmund/carousel.dart';
 export 'package:dortmund/constants.dart';
 export 'package:dortmund/widgets/header.dart';
 export 'package:dortmund/widgets/footer.dart';
@@ -9,11 +8,12 @@ export 'package:dortmund/widgets/center_widget.dart';
 export 'package:dortmund/widgets/action_button.dart';
 export 'package:carousel_slider/carousel_slider.dart';
 export 'package:dortmund/widgets/social_media_icons.dart';
-export 'package:dortmund/state_management/carousel_pages_provider.dart';
+export 'package:dortmund/state_management/pages_provider.dart';
 
 // pub packages
 export 'package:provider/provider.dart';
 export 'package:expandable/expandable.dart';
+export 'package:group_button/group_button.dart';
 export 'package:flutter_bounce/flutter_bounce.dart';
 export 'package:responsive_builder/responsive_builder.dart';
 export 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
