2ac143dec59a0289c8ec23adf9c2e6383f8ad314
Othniel Ussher <othnielussher16@gmail.com>
2021-10-15 06:07:42 +0000

    Remove text field border radius

--- lib/widgets/text_field_decoration.dart
+++ lib/widgets/text_field_decoration.dart
@@ -5,9 +5,11 @@ InputDecoration textFieldDeco({String? hintText}) {
     hintText: hintText,
     hintStyle: const TextStyle(color: Colors.grey),
     enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.zero,
       borderSide: BorderSide(color: Colors.grey.withOpacity(0.3)),
     ),
     focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.zero,
       borderSide: BorderSide(color: Colors.grey.withOpacity(0.3)),
     ),
   );
