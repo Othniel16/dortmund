f325e4a3e149d4a2a60d281c5814d0bddb5781b2
Othniel Ussher <othnielussher16@gmail.com>
2021-10-15 05:52:32 +0000

    Add form data collection

--- lib/pages/hi_there/hi_there.dart
+++ lib/pages/hi_there/hi_there.dart
@@ -34,6 +34,8 @@ class RightWidget extends StatefulWidget {
 }
 
 class _RightWidgetState extends State<RightWidget> {
+  List<String> interests = [];
+  String? name, ageGroup, country, gender, occupation, note, url;
   final String submit = 'SUBMIT';
 
   final String title = 'Hi There ...';
@@ -52,17 +54,20 @@ submission to our quick survey is welcome  ❤''';
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
     return ResponsiveBuilder(builder: (context, sizingInformation) {
       double titleSize =
           sizingInformation.isMobile ? FontSizes.semiLarge : FontSizes.large;
       return SingleChildScrollView(
-        padding: sizingInformation.isMobile
-            ? EdgeInsets.zero
-            : EdgeInsets.only(left: screenWidth / 25),
+        physics: const BouncingScrollPhysics(),
+        padding: EdgeInsets.only(
+          left: screenWidth / 25,
+          right: screenHeight / 25,
+          top: screenHeight / 35,
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height: 30.0),
             Text(
               title,
               style: TextStyle(color: Colors.white, fontSize: titleSize),
@@ -82,82 +87,177 @@ submission to our quick survey is welcome  ❤''';
               width: MediaQuery.of(context).size.width / 3,
             ),
             const SizedBox(height: 30.0),
-            UserInfo(label: 'Age Group', content: ageList(sizingInformation)),
-            const UserInfo(label: 'Country'),
-            const UserInfo(label: 'Gender'),
-            const UserInfo(label: 'Occupation'),
-            const SizedBox(height: 45.0),
-            ListView.builder(
-                shrinkWrap: true,
-                itemCount: 12,
-                physics: const NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return Container(color: Colors.amber);
-                }),
-            ActionButton(label: submit, onTap: onSubmitTap),
+            UserInfoItem(label: 'Name', content: nameInput()),
+            UserInfoItem(label: 'Gender', content: genderList()),
+            UserInfoItem(label: 'Country', onHeaderTap: showCountryList),
+            UserInfoItem(label: 'Age Group', content: ageList()),
+            UserInfoItem(label: 'Occupation', content: occupationInput()),
+            UserInfoItem(label: 'Social media url', content: urlInput()),
+            UserInfoItem(
+                label: 'Interest in women\'s affairs', content: interestList()),
+            UserInfoItem(
+              label: 'Outlook on women\'s empowerment',
+              content: noteInput(),
+            ),
+            const SizedBox(height: 20.0),
+            ArgonButton(
+              height: 35,
+              width: 83,
+              color: Colors.white,
+              child: Text(
+                submit,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              loader: Container(
+                padding: const EdgeInsets.all(10.0),
+                child: const CircularProgressIndicator(
+                  color: Colors.black,
+                  strokeWidth: 1.5,
+                ),
+              ),
+              onTap: (startLoading, stopLoading, btnState) async {
+                if (btnState == ButtonState.Idle) {
+                  startLoading();
+                  await Future.delayed(const Duration(seconds: 3));
+                  stopLoading();
+                  await Future.delayed(const Duration(seconds: 1));
+                  onSubmitTap();
+                }
+              },
+            ),
+            SizedBox(height: screenHeight / 20),
           ],
         ),
       );
     });
   }
 
-  Widget ageList(SizingInformation sizingInformation) {
+  Widget ageList() {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 12.0),
       child: GroupButton(
         spacing: 10.0,
         runSpacing: 8.0,
-        borderRadius: BorderRadius.circular(8.0),
-        groupingType: GroupingType.wrap,
+        buttons: ageGroups,
         selectedColor: SiteColors.brown,
         mainGroupAlignment: MainGroupAlignment.start,
-        onSelected: (index, isSelected) => print('$index button is selected'),
-        buttons: ageGroups,
+        onSelected: (index, isSelected) => ageGroup = ageGroups[index],
+      ),
+    );
+  }
+
+  void showCountryList() {
+    return showCountryPicker(
+      context: context,
+      onSelect: (Country country) => this.country = country.name,
+      countryListTheme: const CountryListThemeData(
+        backgroundColor: Colors.white,
+        borderRadius: BorderRadius.zero,
+        textStyle: TextStyle(fontSize: 16),
+        inputDecoration: InputDecoration(
+          labelText: 'Search',
+          hintText: 'Start typing to search',
+          labelStyle: TextStyle(color: Colors.grey),
+          hintStyle: TextStyle(color: Colors.grey),
+          prefixIcon: Icon(Icons.search, color: Colors.grey),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          disabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget genderList() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      child: GroupButton(
+        spacing: 10.0,
+        runSpacing: 8.0,
+        buttons: genderOptions,
+        selectedColor: SiteColors.brown,
+        mainGroupAlignment: MainGroupAlignment.start,
+        onSelected: (index, isSelected) => gender = genderOptions[index],
+      ),
+    );
+  }
+
+  Widget occupationInput() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      child: TextField(
+        maxLength: 20,
+        onChanged: (value) => occupation = value,
+        decoration: textFieldDeco(hintText: 'Occupation'),
+      ),
+    );
+  }
+
+  Widget nameInput() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      child: TextField(
+        maxLength: 30,
+        keyboardType: TextInputType.name,
+        onChanged: (value) => name = value,
+        decoration: textFieldDeco(hintText: 'Name'),
+      ),
+    );
+  }
+
+  Widget noteInput() {
+    String hintText = 'Note on your outlook on women\'s empowerment';
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      child: TextField(
+        maxLength: 100,
+        onChanged: (value) => note = value,
+        decoration: textFieldDeco(hintText: hintText),
+      ),
+    );
+  }
+
+  Widget urlInput() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      child: TextField(
+        keyboardType: TextInputType.url,
+        onChanged: (value) => url = value,
+        decoration: textFieldDeco(hintText: 'Social media url'),
+      ),
+    );
+  }
+
+  Widget interestList() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      child: GroupButton(
+        spacing: 10,
+        isRadio: false,
+        runSpacing: 8.0,
+        buttons: interestOptions,
+        selectedColor: SiteColors.brown,
+        mainGroupAlignment: MainGroupAlignment.start,
+        onSelected: (index, isSelected) {
+          interests.add(interestOptions[index]);
+        },
       ),
     );
   }
 
   void onSubmitTap() {
     pagesProvider.togglePageIndex(pagesProvider.currentPage + 1);
-  }
-}
-
-class UserInfo extends StatelessWidget {
-  final String label;
-  final Widget? content;
-  const UserInfo({Key? key, required this.label, this.content})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 12.0),
-      child: ExpandableNotifier(
-        controller: ExpandableController(),
-        child: ScrollOnExpand(
-          child: ExpandablePanel(
-            theme: const ExpandableThemeData(
-              hasIcon: true,
-              iconSize: 16.0,
-              expandIcon: Icons.add,
-              collapseIcon: Icons.remove,
-              iconColor: SiteColors.brown,
-              alignment: Alignment.centerLeft,
-              iconPlacement: ExpandablePanelIconPlacement.left,
-              iconPadding: EdgeInsets.only(right: 16.0, top: 2.0),
-            ),
-            expanded: content ?? Container(),
-            collapsed: Container(),
-            header: Text(
-              label,
-              style: const TextStyle(
-                color: SiteColors.brown,
-                fontSize: FontSizes.regular,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
