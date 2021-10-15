b8e65c6c1d9cbb2af3841090e499ee0f966b1c4f
Othniel Ussher <othnielussher16@gmail.com>
2021-10-15 02:58:31 +0000

    Add new constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -9,6 +9,7 @@ class Images {
   static const String instagram = 'assets/images/instagram.png';
   static const String twitter = 'assets/images/twitter.png';
   static const String twoPhones = 'assets/images/two_phones.png';
+  static const String logoAbstract = 'assets/images/logo_abstract.png';
   static const String hiThere = 'assets/images/woman_with_star_and_heart.png';
 }
 
@@ -21,3 +22,12 @@ class FontSizes {
 class SiteColors {
   static const brown = Color(0xFFFFB369);
 }
+
+const List<String> ageGroups = [
+  '13 - 23',
+  '14 - 24',
+  '15 - 25',
+  '26 - 36',
+  '46 - 60',
+  '60+'
+];
