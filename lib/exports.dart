d9fca4d98e39fe7b8e86c310f3423e8a7e097cd0
Othniel Ussher <othnielussher16@gmail.com>
2021-10-19 10:11:36 +0000

    🚢 Add new exports

--- lib/exports.dart
+++ lib/exports.dart
@@ -1,25 +1,24 @@
 export 'package:dortmund/app.dart';
 export 'package:dortmund/home.dart';
-export 'pages/thank_you/thank_you.dart';
 export 'package:dortmund/constants.dart';
 export 'package:dortmund/widgets/header.dart';
 export 'package:dortmund/widgets/footer.dart';
+export 'package:dortmund/widgets/carousel.dart';
 export 'package:dortmund/widgets/center_widget.dart';
 export 'package:dortmund/widgets/action_button.dart';
 export 'package:carousel_slider/carousel_slider.dart';
 export 'package:dortmund/widgets/custom_divider.dart';
 export 'package:dortmund/widgets/social_media_icons.dart';
-export 'package:dortmund/widgets/text_field_decoration.dart';
+export 'package:dortmund/pages/coming_soon/coming_soon.dart';
 export 'package:dortmund/state_management/pages_provider.dart';
-export 'package:dortmund/pages/hi_there/widgets/user_info_item.dart';
 
 // pub packages
 export 'package:provider/provider.dart';
-export 'package:expandable/expandable.dart';
 export 'package:group_button/group_button.dart';
+export 'package:url_launcher/url_launcher.dart';
 export 'package:flutter_bounce/flutter_bounce.dart';
 export 'package:country_picker/country_picker.dart';
 export 'package:responsive_builder/responsive_builder.dart';
 export 'package:flutter_vector_icons/flutter_vector_icons.dart';
-export 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
+export 'package:smooth_page_indicator/smooth_page_indicator.dart';
 export 'package:argon_buttons_flutter/argon_buttons_flutter.dart';
