1bb270031ea5b9f1e17d3f5795e30d997b66bcbe
Othniel Ussher <othnielussher16@gmail.com>
2021-10-19 10:11:20 +0000

    Add new constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,41 +3,34 @@ import 'package:flutter/material.dart';
 class Images {
   static const String aneteLusina = 'assets/images/anete_lusina.jpg';
   static const String logo = 'assets/images/logo.png';
+  static const String black = 'assets/images/black.png';
   static const String disruption = 'assets/images/disruption.jpg';
-  static const String woman = 'assets/images/woman.jpg';
   static const String facebook = 'assets/images/facebook.png';
   static const String instagram = 'assets/images/instagram.png';
   static const String twitter = 'assets/images/twitter.png';
   static const String twoPhones = 'assets/images/two_phones.png';
   static const String logoAbstract = 'assets/images/logo_abstract.png';
-  static const String hiThere = 'assets/images/woman_with_star_and_heart.png';
 }
 
 class FontSizes {
   static const large = 45.0;
   static const semiLarge = 38.0;
   static const regular = 20.0;
+
+  static const largeMobile = 35.0;
+  static const semiLargeMobile = 28.0;
+  static const regularMobile = 18.0;
 }
 
 class SiteColors {
   static const brown = Color(0xFFFFB369);
 }
 
-const List<String> ageGroups = [
-  '13 - 23',
-  '14 - 24',
-  '15 - 25',
-  '26 - 36',
-  '46 - 60',
-  '60+'
-];
+class Urls {
+  static const instagram =
+      'https://www.instagram.com/sistazshare/https://www.instagram.com/sistazshare/';
 
-const List<String> genderOptions = ['Male', 'Female'];
+  static const facebook = 'https://www.facebook.com/sistazshare';
 
-const List<String> interestOptions = [
-  'Writing',
-  'Advocacy',
-  'Counseling',
-  'Empowerment',
-  'Other digital media',
-];
+  static const twitter = 'https://twitter.com/sistazshare';
+}
