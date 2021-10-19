1c6423d615d352af7f02fd3fdda28f4120a928f6
Othniel Ussher <othnielussher16@gmail.com>
2021-10-19 10:11:58 +0000

    Remove slider menu container

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,5 @@
 import 'package:dortmund/exports.dart';
 import 'package:flutter/material.dart';
-import 'package:dortmund/widgets/menu_widget.dart';
-
-final GlobalKey<SliderMenuContainerState> menuKey =
-    GlobalKey<SliderMenuContainerState>();
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -23,20 +19,13 @@ class Home extends StatelessWidget {
             children: [
               const Header(),
               Expanded(
-                child: SliderMenuContainer(
-                  key: menuKey,
-                  hasAppBar: false,
-                  sliderMenu: const MenuWidget(),
-                  slideDirection: SlideDirection.TOP_TO_BOTTOM,
-                  sliderMenuOpenSize: MediaQuery.of(context).size.height,
-                  sliderMain: Consumer<PagesProvider>(
-                    builder: (context, pagesProvider, child) {
-                      return Container(
-                        color: Colors.black,
-                        child: pagesProvider.pages[pagesProvider.currentPage],
-                      );
-                    },
-                  ),
+                child: Consumer<PagesProvider>(
+                  builder: (context, pagesProvider, child) {
+                    return Container(
+                      color: Colors.black,
+                      child: const Carousel(),
+                    );
+                  },
                 ),
               ),
               sizingInformation.isMobile ? Container() : const Footer(),
