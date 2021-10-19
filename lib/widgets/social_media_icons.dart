810de34b777d27f480888a18522fd01cb5476240
Othniel Ussher <othnielussher16@gmail.com>
2021-10-19 10:14:40 +0000

    Add url launching to various social media buttons

--- lib/widgets/social_media_icons.dart
+++ lib/widgets/social_media_icons.dart
@@ -1,9 +1,14 @@
 import 'package:dortmund/exports.dart';
 import 'package:flutter/material.dart';
 
-class SocialMediaIcons extends StatelessWidget {
+class SocialMediaIcons extends StatefulWidget {
   const SocialMediaIcons({Key? key}) : super(key: key);
 
+  @override
+  State<SocialMediaIcons> createState() => _SocialMediaIconsState();
+}
+
+class _SocialMediaIconsState extends State<SocialMediaIcons> {
   final double imageSize = 25.0;
 
   @override
@@ -12,7 +17,7 @@ class SocialMediaIcons extends StatelessWidget {
       children: [
         Bounce(
           duration: const Duration(milliseconds: 100),
-          onPressed: () {},
+          onPressed: () => launchURL(Urls.facebook),
           child: InkWell(
             mouseCursor: SystemMouseCursors.click,
             child: Image.asset(Images.facebook,
@@ -22,7 +27,7 @@ class SocialMediaIcons extends StatelessWidget {
         const SizedBox(width: 18.0),
         Bounce(
           duration: const Duration(milliseconds: 100),
-          onPressed: () {},
+          onPressed: () => launchURL(Urls.instagram),
           child: InkWell(
             mouseCursor: SystemMouseCursors.click,
             child: Image.asset(Images.instagram,
@@ -32,7 +37,7 @@ class SocialMediaIcons extends StatelessWidget {
         const SizedBox(width: 18.0),
         Bounce(
           duration: const Duration(milliseconds: 100),
-          onPressed: () {},
+          onPressed: () => launchURL(Urls.twitter),
           child: InkWell(
             mouseCursor: SystemMouseCursors.click,
             child: Image.asset(Images.twitter,
@@ -41,5 +46,18 @@ class SocialMediaIcons extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  void launchURL(String url) async {
+    await canLaunch(url)
+        ? await launch(url)
+        : ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: const Text('Could not launch url. Please try again'),
+            duration: const Duration(seconds: 3),
+            action: SnackBarAction(
+              label: 'Dismiss',
+              onPressed: () {},
+            ),
+          ));
   }
 }
